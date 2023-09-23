0b749c6f2a7aa120b0f81a03454f1dd45699b804
Navin <naveenj.iteg2022@ssism.org>
2023-09-23 10:45:59 +0530

    intial commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+class Test{
+    public static void main(String[] args) {
+        System.out.println("hello Jenkins!");
+        System.out.println(new Date());
+        
+        
+    }
+}
