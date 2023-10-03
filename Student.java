fb0269f7c2909ae15e0b1687625d16579e6844dd
Navin <naveenj.iteg2022@ssism.org>
2023-10-03 16:08:28 +0530

    seconnd commit

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+   public Student(){
+        System.out.println("Create Student object");
+    }
+
+    public void SayHello(){
+        System.out.println("hello User");
+    }
+}
