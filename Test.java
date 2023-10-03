7ad8a723e368700b58732cfd27fd0430d15e9c73
Navin <naveenj.iteg2022@ssism.org>
2023-10-03 15:58:30 +0530

    main

--- Test.java
+++ Test.java
@@ -3,7 +3,9 @@ import java.util.Date;
 class Test{
     public static void main(String[] args) {
         System.out.println("hello Jenkins!");
+        System.out.println("hello java");
         System.out.println(new Date());
+        System.out.println("one more content");
         
         
     }
