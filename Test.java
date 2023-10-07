92cea598496b6d3945f88a6b36c75477089b65a3
Navin <naveenj.iteg2022@ssism.org>
2023-10-07 12:52:09 +0530

    rebuild

--- Test.java
+++ Test.java
@@ -1,13 +1,14 @@
 import java.util.Date;
 
-class Test{
+class Test {
     public static void main(String[] args) {
         System.out.println("hello Jenkins!");
         System.out.println("hello java");
         System.out.println(new Date());
         System.out.println("one more content");
-        Student student=new Student();
+        Student student = new Student();
         student.SayHello();
-        
+        System.out.println("one more build");
+
     }
 }
