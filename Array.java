08e83551414bc61fe9d3663e74782097924443f9
Navin <naveenj.iteg2022@ssism.org>
2023-10-19 10:46:05 +0530

    update array

--- Array.java
+++ Array.java
@@ -1,5 +1,9 @@
 public class Array {
     public static void main(String[] args) {
-        int arr[]=new int[5];
+        int arr[]={5,6,8,6,89,5,4};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        
     }
 }
