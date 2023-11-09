2368c3a5b3792a3eadaefff8b502b1bed681db9a
Navin <naveenj.iteg2022@ssism.org>
2023-11-09 10:17:28 +0530

    add sub array

--- Array.java
+++ Array.java
@@ -3,7 +3,7 @@ public class Array {
         int arr[]={5,6,8,6,89,5,4};
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]+" ");
-            
+            System.out.println(arr[i]+"  ");
         }
         System.out.println(arr);
     }
